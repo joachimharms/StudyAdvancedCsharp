7e9c06fccf0628021d7b5b74655ce038a704ae4c
joachimharms <joachimharms77@gmail.com>
2017-10-12 22:42:41 +0200

    Add interfaces - done

--- Interfaces/ICustomer.cs
+++ Interfaces/ICustomer.cs
@@ -1,13 +1,4 @@
-﻿#region License
-// Copyright (c) 2017, Joachim Harms
-// All rights reserved.
-// 
-// Unauthorized copying of this file, via any medium is strictly prohibited.
-// Proprietary and confidential.
-// 
-// Written by Joachim Harms
-// <joachimharms.business@gmail.com>
-#endregion
+﻿
 namespace _01_Interfaces
 {
     public interface ICustomer
