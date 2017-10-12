ea66c83eef461bf687a752a439ff15855b85b8af
joachimharms <joachimharms77@gmail.com>
2017-10-12 22:32:07 +0200

    Add Interfaces - done

---
+++ Interfaces/ICustomer.cs
@@ -0,0 +1,28 @@
+﻿#region License
+// Copyright (c) 2017, Joachim Harms
+// All rights reserved.
+// 
+// Unauthorized copying of this file, via any medium is strictly prohibited.
+// Proprietary and confidential.
+// 
+// Written by Joachim Harms
+// <joachimharms.business@gmail.com>
+#endregion
+namespace _01_Interfaces
+{
+    public interface ICustomer
+    {
+        // Felder und Variablen dürfen nicht enthalten sein. Dies ist ein Kompilierfehler:
+        //int Alter;   
+
+        // Eigenschaften dürfen enthalten sein: 
+        int Alter { get; set; }
+
+        // Methoden dürfen enthalten sein:
+        void BuyProduct();  
+
+        // Delegates dürfen enthalten sein.
+        // Events dürfen enthalten sein.
+
+    }
+}
